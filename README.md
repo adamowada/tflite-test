b719c4a3cd56c6d1b7006963573d6704760fd5f5
Adam Owada <adamowada@gmail.com>
2023-11-11 17:41:35 -0800

    created regression function for vercel

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tflite Test
+Adam Owada
+
+### About
+
+Tflite deployment proof of concept on Vercel.
+
+
+### Usage
+
+Make a request like this:
+
+```
+
+```
+
+And you will receive the model's prediction as a response:
+
+```
+
+```
---
+++ api/regression.py
@@ -0,0 +1,49 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import numpy as np
+import tflite_runtime.interpreter as tflite
+
+
+# Load the TFLite model and allocate tensors
+interpreter = tflite.Interpreter(model_path="regression_model.tflite")
+interpreter.allocate_tensors()
+
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+
+# Function to make a prediction
+def predict(number):
+    # Preprocess the input
+    number = float(number)
+    input_data = np.array([[number]], dtype=np.float32)
+
+    # Set the tensor to point to the input data
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    # Run the interpreter
+    interpreter.invoke()
+
+    # Extract the output data
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data[0]
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Take the url query string and create a dictionary of parameters
+        url = self.path
+        url_components = parse.urlsplit(url)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dictionary = dict(query_string_list)  # /?number=something
+
+        # Do stuff
+        if dictionary.get("number"):
+            prediction = predict(dictionary["number"])
+
+        # Forming the response
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(f"The model predicted: {prediction}".encode())
